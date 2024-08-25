8bfd23ba4c2e721025a63ca9069646d33650e3a3
Azizbek Akmatov <azakmatov@gmail.com>
2024-08-26 05:47:05 +0600

    My first progect in CSharp

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 1
+// Console.WriteLine("Hello, World!");
+// 2
+// int value = 100;
+// Console.WriteLine(value);
+// 3
+int value = 42;
+Console.WriteLine(value);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// 4
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+
+double d1 = 6;
+double sum3 = i1 + d1;
+
+int product = i1 * i2;
+Console.WriteLine(sum3);
+
+double product2 = i1 * d1;
+
+Console.WriteLine(product);
+Console.WriteLine(product2);
+
+int quotient = i1 / d1;
+Console.WriteLine(quotient);
