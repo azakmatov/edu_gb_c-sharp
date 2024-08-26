c0946c7f14a40b4f3e91be6b25a8ec9b950954f6
Azizbek Akmatov <azakmatov@gmail.com>
2024-08-26 11:00:22 +0600

    Added Lesson2 and there Task1

--- Lesson1/Task3/Program.cs
+++ Lesson1/Task3/Program.cs
@@ -1,4 +1,4 @@
-﻿int a = 1, b = 2, c = 3, d = 4, e = 5;
+﻿int a = new Random().Next(1, 10), b = new Random().Next(1, 10), c = new Random().Next(1, 10), d = new Random().Next(1, 10), e = new Random().Next(1, 10);
 int max = a;
 if(max < b) { max = b; }
 if(max < c) { max = c; }
---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear.();
+Console.Set.CursorPosition(10, 4)
+Console.WriteLine(“+”);
+int xa = 1, ya = 1,
+xb = 1, yb = 30,
+xc = 40; yc = 30;
+Console.Set.CursorPosition(xa, ya)
+Console.WriteLine(“+”);
+Console.Set.CursorPosition(xb, yb)
+Console.WriteLine(“+”);
+Console.Set.CursorPosition(xс, yс)
+Console.WriteLine(“+”)
