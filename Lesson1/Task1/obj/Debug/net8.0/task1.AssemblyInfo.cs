c189962901223d199ab567420a26042f55349251
Azizbek Akmatov <azakmatov@gmail.com>
2024-08-26 06:03:54 +0600

    I finished my homework

--- Lesson1/Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5abe627fc9aef7f742ede011ee07281cd203bf8")]
-[assembly: System.Reflection.AssemblyProductAttribute("task1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("task1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfd23ba4c2e721025a63ca9069646d33650e3a3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5abe627fc9aef7f742ede011ee07281cd203bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfd23ba4c2e721025a63ca9069646d33650e3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
